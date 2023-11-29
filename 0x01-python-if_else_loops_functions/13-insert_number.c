4c3bbeeea5805d344532656c4885b30cfc6ea8e6
Peter Odero <peterodero561@gmail.com>
2023-11-29 08:03:47 +0000

    Inserteing a node in  a sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -24,7 +24,7 @@ listint_t *insert_node(listint_t **head, int number)
 	{
 		while (current->next != NULL)
 		{
-			if (current->n > number)
+			if (current->next->n > number)
 			{
 				new->next = current->next;
 				current->next = new;
