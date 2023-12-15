9233bb15cb9815d4610232205f1cd2b37d223ec1
Peter Odero <peterodero561@gmail.com>
2023-12-15 10:00:14 +0000

    Printing command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+    if num_args == 0:
+        print("{} arguments.".format("0"))
+    elif num_args == 1:
+        print("{} argument:".format("1"))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i in range(1, num_args + 1):
+            print("{}: {}".format(i, argv[i]))
