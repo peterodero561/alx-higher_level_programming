cd9c228b32763f8a21b0ea632b1339bbb200573f
Peter Odero <peterodero561@gmail.com>
2023-12-15 12:16:41 +0000

    Changing a copy of the list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+    new_list  = my_list[:]
+    if idx < 0:
+        return new_list
+    elif idx > length:
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
+
