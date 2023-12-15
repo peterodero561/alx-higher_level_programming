1eb927d428f15c87fcbe94441847a99d29cb559b
Peter Odero <peterodero561@gmail.com>
2023-12-15 09:22:09 +0000

    Importing more tha one function from a module

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' program that imports functions from the file
+    calculator_1.py, does some Maths, and prints the result.'''
+
+from calculator_1 import mul, add, sub, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
