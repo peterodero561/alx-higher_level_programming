0d5705c5ad6c08dbf85762b5e57d032f8c36655a
Peter Odero <peterodero561@gmail.com>
2024-01-16 09:10:04 +0000

    A class with a private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''this is a class defining a square'''
+
+
+def Square:
+
+    '''
+    a class of a square
+    Atrributer:
+    size - private instance atrribute
+    '''
+
+    def __init__(self, size):
+
+        '''
+        intializes the sides of a square
+        '''
+        self.__size = size
