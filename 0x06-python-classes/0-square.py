e08e6b8398ccd192d9d992707b0c9fe802c0318a
Peter Odero <peterodero561@gmail.com>
2024-01-16 08:24:06 +0000

    Correcting pycodestyle errors

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,11 +4,13 @@ class Square:
     def __init__(self, side):
         '''initializes the side of the square'''
         self.side = side
+
     def area(self):
         '''claculates the area of a given square
         Returns: area of a square
         '''
         return side * side
+
     def perimeter(self):
         '''claculates the perimeter of a given square
         Returns: perimeter of a square
