f1a24724d6c0f191f658ff49970a4600b6b679c8
Peter Odero <peterodero561@gmail.com>
2024-01-16 08:21:07 +0000

    Adding methods to the classSqquare

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 class Square:
     '''An empty class tha defines a square'''
-    pass
+    def __init__(self, side):
+        '''initializes the side of the square'''
+        self.side = side
+    def area(self):
+        '''claculates the area of a given square
+        Returns: area of a square
+        '''
+        return side * side
+    def perimeter(self):
+        '''claculates the perimeter of a given square
+        Returns: perimeter of a square
+        '''
+        return side * 4
