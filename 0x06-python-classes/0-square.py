5185eee1711f02a1291c754338f0afc37f31fff8
Peter Odero <peterodero561@gmail.com>
2024-01-16 08:35:31 +0000

    Correcting pycodestyle

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
 '''this is an class defining a square'''
 
+
 class Square:
     """An empty class tha defines a square
-    Atrributes:
-        side - the length of the sides
-    Methods:
-        area() - claculates area of the square
-        perimeter() - calculates perimeter of the square
     """
-
-    def __init__(self, side):
-        """initializes the side of the square"""
-        self.side = side
-
-    def area(self):
-        """claculates the area of a given square
-        Returns: area of a square
-        """
-        return side * side
-
-    def perimeter(self):
-        """claculates the perimeter of a given square
-        Returns: perimeter of a square
-        """
-        return side * 4
+    pass
