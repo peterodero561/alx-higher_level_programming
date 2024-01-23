19ad24d9c5990731c918e12023dd311ae453a86e
Peter Odero <peterodero561@gmail.com>
2024-01-23 08:06:33 +0000

    Applying static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,6 +85,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        '''return which of a given two rectandle is bigger'''
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
