c199e7369ee3306045232e7d6b2b7e2af7dbcf01
Peter Odero <peterodero561@gmail.com>
2024-01-24 14:29:48 +0000

    Function area in the class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''An empty BaseGeometry class'''
+
+
+class BaseGeometry:
+    '''Empty class'''
+    
+    def area(self):
+        raise Exception("area() is not implemented")
