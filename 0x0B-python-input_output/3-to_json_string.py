8f663a3185da3590e47757488a2fecd11cd71909
Peter Odero <peterodero561@gmail.com>
2024-01-25 11:14:31 +0000

    Returning JSON representaation of a stering

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,6 @@
 '''function that returns the JSON representation of an object (string)'''
 
 
-def to_json_sting(my_obj):
+def to_json_string(my_obj):
     '''returns the JSON representation of anobject'''
     return json.dumps(my_obj)
