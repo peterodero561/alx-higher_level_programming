3dde996af1026818327f6ccf44176de05348eeae
Peter Odero <peterodero561@gmail.com>
2024-01-25 12:17:33 +0000

    json class

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Function that returns dictionary description with simple data structure'''
+import json
+
+
+def class_to_json(obj):
+    '''Returns a dictionary description for JSON serialization of an object.'''
+    json_dict = {}
+
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+
+    return json_dict
