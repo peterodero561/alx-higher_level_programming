7fed30c080bd6ea630604f279b882919e8012fc7
Peter Odero <peterodero561@gmail.com>
2024-01-25 11:08:15 +0000

    Json representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''function that returns the JSON representation of an object (string)'''
+
+
+def to_json_sting(my_obj):
+    '''function that returns the JSON representation of an object (string)'''
+    return json.dumps(my_obj)
