b64c318f2e31fa885e464437d796ad2f7c25725b
Peter Odero <peterodero561@gmail.com>
2024-01-25 10:43:03 +0000

    Reading a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 def read_file(filename=""):
     '''Reads a text file (UTF8) and prints if to stdout'''
-    with open(filename, encoding="utf-8") as f:
-        print(f.read())
+    try:
+        with open(filename, encoding="utf-8") as f:
+            print(f.read())
