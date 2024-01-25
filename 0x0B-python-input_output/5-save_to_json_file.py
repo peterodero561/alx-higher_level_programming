6b21440a7c321dd7536926ef9c14569dbe460e29
Peter Odero <peterodero561@gmail.com>
2024-01-25 11:34:10 +0000

    Saving a sting in json format in afile

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ def save_to_json_file(my_obj, filename):
     '''function that writes an Object to a text file,
     using a JSON representation'''
     with open(filename, "w", encoding="utf-8") as f:
-        json.dumps(my_obj, f)
+        json.dump(my_obj, f)
