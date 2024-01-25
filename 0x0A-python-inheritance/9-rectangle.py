579381330ddb70e8b2f285de82fb05e3b6f4ad74
Peter Odero <peterodero561@gmail.com>
2024-01-25 07:39:42 +0000

    Using __str__ in subclass

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''An empty BaseGeometry class'''
+
+
+class BaseGeometry:
+    '''Empty class'''
+
+    def area(self):
+        '''Raises a Exception'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''validates value'''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be an integer".format(name))
+
+
+class Rectangle(BaseGeometry):
+    '''subclass of BaseGeometry'''
+
+    def __init__(self, width, height):
+        '''Intializes the sides of a Rectangle object'''
+        super().__init__()
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''Calculates the area of a given rectangle object'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
