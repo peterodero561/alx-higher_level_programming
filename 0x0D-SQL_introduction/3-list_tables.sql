f295e1a14212013e0503aadd929ab8c9e4dd91b7
Peter <peterodero561@gmail.com>
2024-02-15 00:45:25 -0800

    fix: all: corrected syntax error

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,13 +1,13 @@
 -- #!/usr/bin/env bash
 -- a script that lists all tables of a database in sql server
 
-DELIMETER $$
+DELIMITER $$
 CREATE PROCEDURE ListTables(IN dbName VARCHAR(255))
 BEGIN
 	SET @query = CONCAT("USE", dbName, ";");
 	PREPARE stmt FROM @query;
 	EXECUTE stmt;
-	DEALLOCATE PREAPARE stmt;
+	DEALLOCATE PREPARE stmt;
 	SHOW TABLES;
 END$$
-DELIMETER ;
+DELIMITER ;
