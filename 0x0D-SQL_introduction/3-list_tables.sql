e41ec448e46a9478f3e0ae3521f401c44f5c6842
Peter <peterodero561@gmail.com>
2024-02-15 00:58:14 -0800

    feat: added a statement to check and drop if the variable am using has been created

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -2,6 +2,7 @@
 -- a script that lists all tables of a database in sql server
 
 DELIMITER $$
+DROP PROCEDURE IF EXISTS ListTables;
 CREATE PROCEDURE ListTables(IN dbName VARCHAR(255))
 BEGIN
 	SET @query = CONCAT("USE", dbName, ";");
