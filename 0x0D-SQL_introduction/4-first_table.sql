3b5491eda620a3a197a69d6876ec66f989cec3f6
Peter <peterodero561@gmail.com>
2024-02-15 01:58:17 -0800

    fix: changed the logic

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,13 +1,14 @@
 -- A script that creates a table called "first_table" in caurrent database
 -- it has id and name
 
-DELIMITER $$
-CREATE PROCEDURE CreateFirstTable()
-BEGIN
-	-- DECLARE CONTINUE HANDLER FOR SQLEXEPTION BEGIN END;
+-- DELIMITER $$
+-- DROP PROCEDURE IF EXISTS CreateFirstTable;
+-- CREATE PROCEDURE CreateFirstTable()
+-- BEGIN
+--	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
 	CREATE TABLE IF NOT EXISTS first_table (
 		id INT,
 		name VARCHAR(256)
 	);
-END$$
-DELIMITER ;
+-- END$$
+-- DELIMITER ;
