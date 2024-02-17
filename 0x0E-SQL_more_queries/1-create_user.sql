6e073fea5a25c7f1477a7692c41a00ff472f1bcf
Peter <peterodero561@gmail.com>
2024-02-17 02:04:25 -0800

    build: all: a script to create a user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- creates a user user_0d_1
+-- if user exists it should not fail
+-- user_od_1 password is user_0d_1_pwd
+-- user_0d_1 should have all privileges
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGE ON *.* TO 'user_od_1'localhost';
