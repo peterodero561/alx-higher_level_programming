9c3e4d15d4c0a698e60cc2e8cdb0fb0296b09693
peterodero561 <peterodero561@gmail.com>
2024-03-13 20:09:20 +0000

    fix: changed code to stop using length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('process');
-if (argv.length <= 2) {
-  console.log('No argument');
+if (process.argv !== undefined) {
+  console.log(process.argv[2]);
 } else {
-  console.log(argv[2]);
+  console.log('No argument');
 }
