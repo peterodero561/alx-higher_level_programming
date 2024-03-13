c15e8bc3a745ab2a22e7cc94c3767acca119a699
peterodero561 <peterodero561@gmail.com>
2024-03-13 21:23:54 +0000

    build: all: created  a script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function fact (num) {
+  if (num === 1) {
+    return 1;
+  }
+  return num * fact(num - 1);
+}
+
+if (process.argv.length <= 2) {
+  console.log(1);
+} else {
+  console.log(fact(parseInt(process.argv[2])));
+}
