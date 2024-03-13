a7afaab401e38ec8f258c471626dd90157913a91
peterodero561 <peterodero561@gmail.com>
2024-03-13 21:06:00 +0000

    fix: corrected the error message

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,5 +2,5 @@
 if (process.argv !== undefined && Number.isInteger(parseInt(process.argv[2])) && Number.isInteger(parseInt(process.argv[3]))) {
   console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
 } else {
-  console.log('Nan');
+  console.log('NaN');
 }
