f4f7cf13526bc00145aa286cba3951c8a2d4f662
peterodero561 <peterodero561@gmail.com>
2024-03-14 10:34:29 +0000

    fix: changed the code to use side initialized in Square class in charPrint()

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,15 +3,16 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (s) {
     super(s, s);
+    this.side = s;
   }
 
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.width; i++) {
+    for (let i = 0; i < this.side; i++) {
       let row = '';
-      for (let j = 0; j < this.width; j++) {
+      for (let j = 0; j < this.side; j++) {
         row += c;
       }
       console.log(row);
