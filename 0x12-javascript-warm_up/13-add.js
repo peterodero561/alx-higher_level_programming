ec66620ba9f2e160a4b0357493b6c6a93489653c
peterodero561 <peterodero561@gmail.com>
2024-03-14 05:23:11 +0000

    fix: correcting syntax

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
 // Define the function
-function add(a, b) {
+exports.add = function (a, b) {
     return a + b;
-}
-
-// Export the function for Node.js
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = add;
-}
-
-// Export the function for browser environment
-if (typeof window !== 'undefined') {
-    window.add = add;
 }
