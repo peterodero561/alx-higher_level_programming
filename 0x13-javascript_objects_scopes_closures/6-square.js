2a116a04ace639265d0942c24e7849f8c51f89ed
peterodero561 <peterodero561@gmail.com>
2024-03-14 10:32:48 +0000

    fix: changed the code to use width in charPrint()

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,9 +9,9 @@ class Square extends Rectangle {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.height; i++) {
+    for (let i = 0; i < this.width; i++) {
       let row = '';
-      for (let j = 0; j < this.height; j++) {
+      for (let j = 0; j < this.width; j++) {
         row += c;
       }
       console.log(row);
