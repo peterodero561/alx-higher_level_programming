7c11abd5aab6b43d159b363c9a1450c0b6dbb686
peterodero561 <peterodero561@gmail.com>
2024-03-14 10:38:58 +0000

    fix: corrected the parent class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
+const SquareParent = require('./5-square');
+class Square extends SquareParent {
   constructor (s) {
-    super(s, s);
+    super(s);
     this.side = s;
   }
 
