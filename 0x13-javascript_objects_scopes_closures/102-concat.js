6e405346b6e272722af5c13a1946e582821bea9e
peterodero561 <peterodero561@gmail.com>
2024-03-15 10:03:55 +0000

    fix: corrected semistarndard errors

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,26 +1,24 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function concatFiles(file1Path, file2Path, destinationPath) {
-        // Read the content of the first file
-        const data1 = fs.readFileSync(file1Path, 'utf8');
-        
-        // Read the content of the second file
-        const data2 = fs.readFileSync(file2Path, 'utf8');
-        
-        // Concatenate the contents of both files
-        const concatenatedData = data1 + data2;
-        
-        // Write the concatenated data to the destination file
-        fs.writeFileSync(destinationPath, concatenatedData);
-        
-        
+function concatFiles (file1Path, file2Path, destinationPath) {
+  // Read the content of the first file
+  const data1 = fs.readFileSync(file1Path, 'utf8');
+
+  // Read the content of the second file
+  const data2 = fs.readFileSync(file2Path, 'utf8');
+
+  // Concatenate the contents of both files
+  const concatenatedData = data1 + data2;
+
+  // Write the concatenated data to the destination file
+  fs.writeFileSync(destinationPath, concatenatedData);
 }
 
 // Check if arguments are provided
 if (process.argv.length !== 5) {
-    console.error("Usage: node script.js <file1Path> <file2Path> <destinationPath>");
-    process.exit(1);
+  console.error('Usage: node script.js <file1Path> <file2Path> <destinationPath>');
+  process.exit(1);
 }
 
 const file1Path = process.argv[2];
