a5bc47741aac7ef7a430c38dc0aaef98ad817ae9
peterodero561 <peterodero561@gmail.com>
2024-03-16 15:10:37 +0000

    perf: new approch

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
-'''Write a script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa:'''
-
+'''function to list states starting with N'''
 import sys
 import MySQLdb
 
 
-def listStatesN(userName, password, database):
-    '''function to list states starting with N'''
-    # connect to database
+def listStatesN(username, password, database):
+    '''Function to list states starting with N'''
+    # Connect to the database
     db = MySQLdb.connect(
-            'host=localhost', port=33306, user=username,
+            host='localhost', port=3306, user=username,
             passwd=password, db=database)
 
-    # create cursor object
+    # Create a cursor object
     cursor = db.cursor()
 
-    # SQL statement to get states
-    cursor.execute(
-            "SELECT * FROM states "
-            "WHERE states.name LIKE 'N%' ORDER BY states.id ASC")
+    # Execute SQL query to retrieve states starting with 'N' and ordered by ID
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch and print all the states
+    # Fetch all the matching states
     states = cursor.fetchall()
+
+    # Display the results
     for state in states:
         print(state)
 
-    # close cursor and database
+    # Close cursor and database connection
     cursor.close()
     db.close()
 
 
-if __name__ == '__main__':
-    if (len(sys.argv) < 4):
-        print("Fewer arguments")
+if __name__ == "__main__":
+    # Check if all three arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: python3 script.py <username> <password> <database>")
         sys.exit(1)
 
+    # Extract arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # call function to list states
+    # Call the function to list states
     listStatesN(username, password, database)
