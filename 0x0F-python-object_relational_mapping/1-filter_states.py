b75d99f675d3b4ae77a2f926fa249c00369b00c4
peterodero561 <peterodero561@gmail.com>
2024-03-16 14:49:26 +0000

    Build: all: created a python script that lists all states in a given database starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''Write a script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa:'''
+
+import sys
+import MySQLdb
+
+
+def listStatesN(userName, password, database):
+    '''function to list states starting with N'''
+    # connect to database
+    db = MySQLdb.connect(
+            'host=localhost', port=33306, user=username,
+            passwd=password, db=database)
+
+    # create cursor object
+    cursor = db.cursor()
+
+    # SQL statement to get states
+    cursor.execute(
+            "SELECT * FROM states
+            WHERE state.name LIKE 'N%' ORDER BY states.id ASC")
+
+    # Fetch and print all the states
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    # close cursor and database
+    cursor.close()
+    db.close()
+
+
+if '__name__' = '__main__':
+    if (len(sys.argv) < 4):
+        print("Fewer arguments")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # call function to list states
+    listStatesN(username, password, database)
