a62dfbbe91445a46d3b522abe4f557ba498a0b25
Peter <peterodero561@gmail.com>
2024-03-17 12:43:13 -0700

    fix: corrected pycodestyle errors

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def listFirstState(username, password, database):
     '''prints first state of a database'''
     # create engine
