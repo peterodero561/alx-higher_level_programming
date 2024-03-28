e398e7a646d2da3eeb056c077fc5ff21b644d90c
Peter <peterodero561@gmail.com>
2024-03-28 08:38:37 -0700

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends GET to URL and displays body of responce
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1"
+curl -s -o /dev/null -w "%{http_code}\n" $1 | grep -q 200 && curl -s $1
