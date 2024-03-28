2ec931da9911225269fa7c284375b4f74307e821
Peter <peterodero561@gmail.com>
2024-03-28 07:45:19 -0700

    Perf: chamged to previous version

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send a request to a URL and display the size of the body of th responce
-curl -sI "$1" | wc -c
+# Script to send a request to a URL and display the size of the body of th responc
+curl -sI "$1" | grep -i "content-length" | awk '{print $2}'
