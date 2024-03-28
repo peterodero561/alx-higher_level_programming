8a793fc0cba70c379a50e2a5059972061d9975e0
Peter <peterodero561@gmail.com>
2024-03-28 08:31:23 -0700

    perf: new approach 3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends GET to URL and displays body of responce
-curl -s "$1" | sed -n '/<body>/,/<\/body>/p'
+[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1"
