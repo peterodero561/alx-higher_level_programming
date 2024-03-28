4fb470231f5dd258ffd62913702c8b487ba955ef
Peter <peterodero561@gmail.com>
2024-03-28 08:05:37 -0700

    fix: removed an extra semi-colon

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends GET to URL and displays body of responce
-curl -s "$1" | { read -r _; [ "${REPLY%% *}" = "200" ] && cat || echo "Error: Received HTTP status code ${REPLY%% *}"; }
+curl -s "$1" | { read -r _; [ "${REPLY%% *}" = "200" ] && cat; }
