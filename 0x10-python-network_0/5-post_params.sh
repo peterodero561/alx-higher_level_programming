623f84f25dcf2f787a957e6485f5b8bae418fbd2
Peter <peterodero561@gmail.com>
2024-03-28 09:22:31 -0700

    fix: error in excess quotes

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # senda a POST to URL and displays the response
-curl -s -X POST -d ""email=test@gmail.com&subject=I+will+always+be+here+for+PLD"" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I+will+always+be+here+for+PLD" "$1"
