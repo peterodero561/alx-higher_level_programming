29ed53f05c5db47a079da9bd606e3e6d8066d940
Peter <peterodero561@gmail.com>
2024-03-28 11:30:27 -0700

    Avoiding using redirection

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # request given URL and displays status code of the response
-curl -s -o /tmp/response.txt -w "%{http_code}" "$1" && cat /tmp/response.txt && rm /tmp/response.txt
+response=$(curl -s -o /tmp/response.txt -w "%{http_code}" "$1") && echo "$response" | tail -n1 && rm /tmp/response.txt
